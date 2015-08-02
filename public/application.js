78f419e99364313bc603b66a40f3fef8ff6a5772
Anil Dukkipatty <anildukkipatty@gmail.com>
2015-08-02 11:01:08 +0530

    blank 3

--- public/application.js
+++ public/application.js
@@ -290,15 +290,41 @@ function notify(msg, status, time) {
 	'use strict';
 	
 	angular.module('BrsManager.campaigns')
-	.controller('campaigns.ProfileCtrl', ['$scope', 'Campaign', '$routeParams',
-		function ($scope, Campaign, $routeParams) {
+	.controller('campaigns.ProfileCtrl', ['$scope', 'Campaign', '$routeParams', '$http', '$interval',
+		function ($scope, Campaign, $routeParams, $http, $interval) {
 			
+			var intervalPromise;
 			Campaign.get({id: $routeParams.id}).$promise
 				.then(function (data) {
 					$scope.campaign = data;
 
-					$scope.progress = (parseFloat($scope.campaign.raised)/parseFloat($scope.campaign.money)) * 100;
+					setProgress();
+					intervalPromise = $interval(repeat, 4000);
+					
 				});
+
+			function repeat () {
+				$http.get('https://api.blockcypher.com/v1/btc/test3/addrs/' + $scope.campaign.address + '/balance?token=bf121c744dce06d0ec4495fac2975ce9')
+					.success(function (data) {
+						if (data.error) console.log(data);
+						else {
+							var balance = data.balance * 0.00000001;
+							$scope.campaign.raised = balance;
+							setProgress();
+							Campaign.update({id:$routeParams.id}, {raised: balance}).$promise;
+						}
+					});
+			}	
+
+			function setProgress () {
+				$scope.progress = (parseFloat($scope.campaign.raised)/parseFloat($scope.campaign.money)) * 100;
+			}
+
+			$scope.$on('$destroy',function(){
+			    if(intervalPromise)
+			        $interval.cancel(intervalPromise);   
+			});
+			
 
 		}
 	]);
